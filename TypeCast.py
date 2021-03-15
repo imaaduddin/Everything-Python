e22e7f239e49f1ba97d68667b453a1fd9feca011
Imaad Uddin <imaaduddin@icloud.com>
2021-03-15 12:51:28 -0500

    type casting

---
+++ TypeCast.py
@@ -0,0 +1,15 @@
+# type casting is convert the data type of a value to another data type 
+
+x = 1 # int
+y = 2.0 # float
+z = "3" # str
+
+x = float(x)
+y = int(y)
+z = int(z)
+
+print(x)
+print(y)
+print(z)
+
+print("X is: " + str(x))
