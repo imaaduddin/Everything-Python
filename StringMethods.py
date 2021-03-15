70d52054444029d48db0f50557fb46e93e814ac2
Imaad Uddin <imaaduddin@icloud.com>
2021-03-15 12:42:23 -0500

    string methods

---
+++ StringMethods.py
@@ -0,0 +1,12 @@
+name = "Naruto Uzumaki"
+
+print(len(name)) # gives you the length of string
+print(name.find("N")) # finds the index of charcter within a string
+print(name.capitalize()) # capitalizes first letter of the string 
+print(name.upper()) # capitalizes the whole string 
+print(name.lower()) # makes the whole string lower case
+print(name.isdigit()) # will tell you if string is digits 
+print(name.isalpha()) # tells you if your string has alphabetial letters, will tell you false if their is a space
+print(name.count("a")) # tells you how many times a character is within a string 
+print(name.replace("a", "e")) # replaces one charcter with another, this first charcter is the charcter you want replaced with the second charcter within the method
+print(name * 3) # prints the string the number of times you'd like in the method
