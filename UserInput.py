da2e8c20c0dde8f861554ac9b57fd2dc4a987c6e
Imaad Uddin <imaaduddin@icloud.com>
2021-03-15 13:02:17 -0500

    user input

---
+++ UserInput.py
@@ -0,0 +1,8 @@
+name = input("What is your name?: ")
+age = int(input("What is your age?: "))
+height = float(input("How tall are you?: "))
+
+
+print("Hello " + name)
+print("You are " +str(age) +" years old!")
+print("You are " +str(height) + " cm tall!")
