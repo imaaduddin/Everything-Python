47ab2fb9c4a90d9ad8747ceb74e72db60d82a349
Imaad Uddin <imaaduddin@icloud.com>
2021-03-16 15:06:43 -0500

    if else statements

---
+++ IfStatements.py
@@ -0,0 +1,11 @@
+# if statement = a block of code that will execute if it's condition is true 
+
+age = int(input("How old are you? "))
+
+if age >= 18:
+  print("You are old enough to enter!")
+elif age < 0:
+  print("You're not even alive LOL!")
+else:
+  print("You are not old enough!")
+
