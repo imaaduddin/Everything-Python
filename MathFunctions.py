0bfc5dfe2b24b86acee17d031099a3c3dee13263
Imaad Uddin <imaaduddin@icloud.com>
2021-03-16 14:16:13 -0500

    math functions

---
+++ MathFunctions.py
@@ -0,0 +1,16 @@
+import math
+
+pi = 3.14
+x = 7
+y = 5.67
+z = 1.23
+
+
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi, 2))
+# print(math.sqrt(pi))
+# print(max(x, y, z, pi))
+print(min(x, pi, y , z))
