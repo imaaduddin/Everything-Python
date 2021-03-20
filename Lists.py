22c1ed2de3904ada2f9e130738294fb489b41920
Imaad Uddin <imaaduddin@icloud.com>
2021-03-19 19:32:10 -0500

    lists

---
+++ Lists.py
@@ -0,0 +1,13 @@
+# list = used to store multiple items in a single variable
+
+food = ["sushi", "pizza", "burgers", "tacos", "pasta"]
+
+food[4] = "ramen" 
+# food.append("kabob")
+# food.remove("ramen")
+# food.pop()
+# food.insert(0, "steak")
+# food.sort()
+food.clear()
+
+print(food)
