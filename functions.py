c63dfd537a85dab4d3533b6729071c75df8598a5
Imaad Uddin <imaaduddin@icloud.com>
2021-03-20 14:37:12 -0500

    functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+# function = a block of code which is executed only when it is called.
+
+def hello (name, age):
+  print("Hello " + name)
+  print("You are " + str(age) + " years old")
+
+first_name = "Naruto"
+current_age = 19
+hello(first_name, current_age)
