b7fc781f1a4f1038a8f16295b4e0ed1b2415bba7
Imaad Uddin <imaaduddin@icloud.com>
2021-03-19 19:52:13 -0500

    2D lists

---
+++ 2DLists.py
@@ -0,0 +1,9 @@
+# 2D lists = a list of lists 
+
+drinks = ["coffee", "soda", "boba"]
+dinner = ["sushi", "ramen", "kabobs"]
+dessert = ["cheesecake", "cookies", "ice cream"]
+
+food = [drinks, dinner, dessert]
+
+print(food[0][1])
