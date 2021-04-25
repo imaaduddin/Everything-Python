e8a62104c229f60807a17572167937b43e30ae17
Imaad Uddin <imaaduddin@icloud.com>
2021-04-25 00:03:04 -0500

    modukes

--- modules.py
+++ modules.py
@@ -1,3 +1,7 @@
 # modules = a file comtaining python code, may contain functions, classes, etc
 # used with modular programming, which is to separate programs into aparts 
 
+import modules_two as greeting
+
+greeting.hello()
+greeting.bye()
---
+++ modules_two.py
@@ -0,0 +1,5 @@
+def hello():
+  print("Hi! Welcome to Konoha!")
+
+def bye():
+  print("Thank you for visiting Konoha! Come back again!")
