b182750cb245a9963a017bb5abdb3c5505e07b06
Imaad Uddin <imaaduddin@icloud.com>
2021-04-25 00:13:34 -0500

    rock paper and scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,10 @@
+import random 
+
+choices = ["rock", "paper", "scissors"]
+
+computer = random.choice(choices)
+
+player = input("rock, paper or scissors?: ")
+
+print("computer: ", computer)
+print("player: ", player)
